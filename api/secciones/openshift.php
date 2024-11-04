18b0bdae6c5c4469ad362101b3ce5314562bd801
MarlonSalinasCastillo <126306127+MarlonSalinasCastillo@users.noreply.github.com>
2024-11-04 10:25:02 -0500

    Update openshift.php

--- api/secciones/openshift.php
+++ api/secciones/openshift.php
@@ -1,11 +1,11 @@
 <?php
 /*****
-SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
+SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERÉS EN ESTA CATEGORIA
 *****/
 
 $openshift = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Lanzamiento de nuevas características en OpenShift 4.10",
+    "autor" => "María Fernández",
+    "resumen" => "Red Hat ha anunciado el lanzamiento de OpenShift 4.10, que incluye mejoras en la seguridad, optimización de recursos y una nueva interfaz de usuario para facilitar la gestión de contenedores.",
 ];
 ?>
