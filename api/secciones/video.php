e3b2b3bd1ab34e896c439ae0ed12002996e92fce
MarlonSalinasCastillo <126306127+MarlonSalinasCastillo@users.noreply.github.com>
2024-11-04 10:26:45 -0500

    Update video.php

--- api/secciones/video.php
+++ api/secciones/video.php
@@ -1,11 +1,11 @@
 <?php
 /*****
-SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
+SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERÉS EN ESTA CATEGORÍA
 *****/
 
 $video = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "El auge de las plataformas de streaming en 2024",
+    "autor" => "Ana Beltrán",
+    "resumen" => "Las plataformas de streaming continúan dominando el mercado del entretenimiento, con un aumento en la producción de contenido original y la expansión a nuevos mercados.",
 ];
 ?>
