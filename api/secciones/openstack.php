018bf316c73733f1fd7061f82bf603db371a3e81
MarlonSalinasCastillo <126306127+MarlonSalinasCastillo@users.noreply.github.com>
2024-11-04 10:26:13 -0500

    Update openstack.php

--- api/secciones/openstack.php
+++ api/secciones/openstack.php
@@ -1,11 +1,11 @@
 <?php
 /*****
-SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
+SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERÉS EN ESTA CATEGORÍA
 *****/
 
 $openstack = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "OpenStack Summit 2024: Innovaciones en la nube",
+    "autor" => "Fernando Ruiz",
+    "resumen" => "El OpenStack Summit 2024 ha presentado nuevas herramientas y mejoras en la plataforma, enfocándose en la interoperabilidad y la eficiencia en la gestión de recursos en la nube.",
 ];
 ?>
