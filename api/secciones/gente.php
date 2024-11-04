7cb90afe4a8a4b7d14742c46d3097c3715f68b49
MarlonSalinasCastillo <126306127+MarlonSalinasCastillo@users.noreply.github.com>
2024-11-04 10:22:56 -0500

    Update gente.php

--- api/secciones/gente.php
+++ api/secciones/gente.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $gente = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Reconocimiento a jóvenes líderes comunitarios",
+    "autor" => "Lucía Martínez",
+    "resumen" => "Un grupo de jóvenes de diversas comunidades ha sido premiado por su labor en iniciativas de sostenibilidad y ayuda social, inspirando a otros a seguir su ejemplo.",
 ];
 ?>
