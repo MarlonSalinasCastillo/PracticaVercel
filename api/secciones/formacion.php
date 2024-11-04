a07a9a0ba7c29200a208330a3002fcc47c3af1c9
MarlonSalinasCastillo <126306127+MarlonSalinasCastillo@users.noreply.github.com>
2024-11-04 10:23:53 -0500

    Update formacion.php

--- api/secciones/formacion.php
+++ api/secciones/formacion.php
@@ -1,11 +1,11 @@
 <?php
 /*****
-SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
+SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERÉS EN ESTA CATEGORIA
 *****/
 
 $formacion = [
-"titulo" => "XXX",
-"autor" => "sistemas@uns",
-"resumen" => "XXX",
+    "titulo" => "Nuevos cursos en línea para el desarrollo profesional",
+    "autor" => "sistemas@uns",
+    "resumen" => "La universidad ha lanzado una serie de cursos en línea diseñados para mejorar las habilidades profesionales y facilitar el aprendizaje remoto en diversas áreas.",
 ];
 ?>
