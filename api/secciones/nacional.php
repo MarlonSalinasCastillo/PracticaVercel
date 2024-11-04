a505d8dcec10e0a8897a9f39599599588d806b1f
MarlonSalinasCastillo <126306127+MarlonSalinasCastillo@users.noreply.github.com>
2024-11-04 10:20:13 -0500

    Update nacional.php

--- api/secciones/nacional.php
+++ api/secciones/nacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $nacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Aumento del salario mínimo en 2024",
+    "autor" => "Ana Torres",
+    "resumen" => "El gobierno ha anunciado un aumento del 10% en el salario mínimo a partir de enero de 2024, buscando mejorar la calidad de vida de los trabajadores.",
 ];
 ?>
