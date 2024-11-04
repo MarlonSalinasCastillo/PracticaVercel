4c9046ed069f8be88875e4df7e580d7af54394e4
MarlonSalinasCastillo <126306127+MarlonSalinasCastillo@users.noreply.github.com>
2024-11-04 10:21:27 -0500

    Update tecnologia.php

--- api/secciones/tecnologia.php
+++ api/secciones/tecnologia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $tecnologia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Lanzamiento del nuevo smartphone con inteligencia artificial",
+    "autor" => "Sofía Reyes",
+    "resumen" => "La última generación de smartphones incluye un asistente de inteligencia artificial que promete mejorar la experiencia del usuario al anticipar sus necesidades.",
 ];
 ?>
