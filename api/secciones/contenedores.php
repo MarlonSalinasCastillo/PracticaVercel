463c7bc25c9a252e8a9c2e07b46199a98aaf8a14
MarlonSalinasCastillo <126306127+MarlonSalinasCastillo@users.noreply.github.com>
2024-11-04 10:25:52 -0500

    Update contenedores.php

--- api/secciones/contenedores.php
+++ api/secciones/contenedores.php
@@ -1,11 +1,11 @@
 <?php
 /*****
-SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
+SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERÉS EN ESTA CATEGORÍA
 *****/
 
 $contenedores = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Tendencias en el uso de contenedores en 2024",
+    "autor" => "Laura Gómez",
+    "resumen" => "El uso de contenedores sigue en aumento, con empresas adoptando tecnologías de orquestación como Kubernetes para mejorar la eficiencia y escalabilidad en sus infraestructuras.",
 ];
 ?>
