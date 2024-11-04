6cb030561ba8efad85f3620446497d03797b94f6
MarlonSalinasCastillo <126306127+MarlonSalinasCastillo@users.noreply.github.com>
2024-11-04 10:21:55 -0500

    Update ciencia.php

--- api/secciones/ciencia.php
+++ api/secciones/ciencia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $ciencia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Descubrimiento de una nueva especie en la Amazonía",
+    "autor" => "Dr. Elena Fernández",
+    "resumen" => "Un equipo de investigadores ha identificado una nueva especie de rana en la Amazonía, destacando la importancia de conservar los ecosistemas tropicales.",
 ];
 ?>
