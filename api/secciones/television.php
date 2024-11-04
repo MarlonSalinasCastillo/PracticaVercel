f7b725687b3c33be05a0a5a9739c192aff10516f
MarlonSalinasCastillo <126306127+MarlonSalinasCastillo@users.noreply.github.com>
2024-11-04 10:23:25 -0500

    Update television.php

--- api/secciones/television.php
+++ api/secciones/television.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $television = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Éxito de la nueva serie de ciencia ficción en streaming",
+    "autor" => "Carlos Pérez",
+    "resumen" => "La nueva serie de ciencia ficción ha conquistado a la audiencia, alcanzando los primeros lugares en las plataformas de streaming y generando conversaciones en redes sociales.",
 ];
 ?>
