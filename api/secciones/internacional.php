30cba6b230cab4de20122d3f9cc654cd6974f364
MarlonSalinasCastillo <126306127+MarlonSalinasCastillo@users.noreply.github.com>
2024-11-04 10:19:54 -0500

    Update internacional.php

--- api/secciones/internacional.php
+++ api/secciones/internacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $internacional = [
-"titulo" => "Internacional",
-"autor" => "Gabriel Garcia Marquez",
-"resumen" => "Esto es un texto",
+    "titulo" => "Cumbre Mundial sobre el Cambio Climático 2024",
+    "autor" => "Gabriel García Márquez",
+    "resumen" => "Líderes de todo el mundo se reúnen en 2024 para discutir medidas urgentes contra el cambio climático y promover acciones sostenibles.",
 ];
 ?>
