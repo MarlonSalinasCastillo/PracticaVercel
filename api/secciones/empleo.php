7da833ac320a5ded1d12f7725bf40c990769ef36
MarlonSalinasCastillo <126306127+MarlonSalinasCastillo@users.noreply.github.com>
2024-11-04 10:24:20 -0500

    Update empleo.php

--- api/secciones/empleo.php
+++ api/secciones/empleo.php
@@ -1,11 +1,11 @@
 <?php
 /*****
-SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
+SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERÉS EN ESTA CATEGORIA
 *****/
 
 $empleo = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Incremento en las oportunidades de trabajo en el sector tecnológico",
+    "autor" => "Javier Sánchez",
+    "resumen" => "El sector tecnológico ha visto un aumento significativo en la creación de empleos, impulsado por la demanda de habilidades digitales y el crecimiento de startups en el país.",
 ];
 ?>
