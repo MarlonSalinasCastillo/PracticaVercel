6320f429bf29bba9718496c757337ca0f16aae61
MarlonSalinasCastillo <126306127+MarlonSalinasCastillo@users.noreply.github.com>
2024-11-04 10:24:44 -0500

    Update sociedad.php

--- api/secciones/sociedad.php
+++ api/secciones/sociedad.php
@@ -1,11 +1,11 @@
 <?php
 /*****
-SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
+SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERÉS EN ESTA CATEGORIA
 *****/
 
 $sociedad = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Aumento de la participación ciudadana en las elecciones",
+    "autor" => "Clara Gómez",
+    "resumen" => "Las recientes elecciones mostraron un aumento en la participación ciudadana, con un 75% de votantes, lo que refleja un creciente interés por el compromiso cívico.",
 ];
 ?>
