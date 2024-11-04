1c82fbdc60b59029c50461bf80eaa3834cca6fe5
MarlonSalinasCastillo <126306127+MarlonSalinasCastillo@users.noreply.github.com>
2024-11-04 10:19:16 -0500

    Update economia.php

--- api/secciones/economia.php
+++ api/secciones/economia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $economia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "El crecimiento económico de América Latina en 2024",
+    "autor" => "María López",
+    "resumen" => "Se prevé que la economía de América Latina crezca un 2.5% en 2024, impulsada por el aumento en las exportaciones y la inversión en infraestructura.",
 ];
 ?>
