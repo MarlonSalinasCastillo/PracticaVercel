35d4be4fcb588e9f114d6df28215fd1a67b5a5d7
MarlonSalinasCastillo <126306127+MarlonSalinasCastillo@users.noreply.github.com>
2024-11-04 10:20:49 -0500

    Update opinion.php

--- api/secciones/opinion.php
+++ api/secciones/opinion.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $opinion = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "La importancia de la educación en tiempos de crisis",
+    "autor" => "Laura Martínez",
+    "resumen" => "En un mundo marcado por la incertidumbre, la educación se presenta como la clave para un futuro mejor. Es necesario invertir en ella para preparar a las nuevas generaciones ante los desafíos que enfrentamos.",
 ];
 ?>
