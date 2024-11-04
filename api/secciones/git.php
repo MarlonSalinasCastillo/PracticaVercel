e10254cc0bf5844fc82b32075c749003d6649763
MarlonSalinasCastillo <126306127+MarlonSalinasCastillo@users.noreply.github.com>
2024-11-04 10:25:21 -0500

    Update git.php

--- api/secciones/git.php
+++ api/secciones/git.php
@@ -1,11 +1,11 @@
 <?php
 /*****
-SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
+SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERÉS EN ESTA CATEGORIA
 *****/
 
 $git = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Nuevas funcionalidades en Git 2.39",
+    "autor" => "Carlos Torres",
+    "resumen" => "La última versión de Git, la 2.39, introduce nuevas funcionalidades que mejoran la gestión de ramas y la colaboración en proyectos, facilitando un flujo de trabajo más eficiente.",
 ];
 ?>
