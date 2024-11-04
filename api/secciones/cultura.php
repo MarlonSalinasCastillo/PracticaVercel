93386c8bef6317f6b2ee3e64e69178fb1d591120
MarlonSalinasCastillo <126306127+MarlonSalinasCastillo@users.noreply.github.com>
2024-11-04 10:22:24 -0500

    Update cultura.php

--- api/secciones/cultura.php
+++ api/secciones/cultura.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $cultura = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "La restauración del mural de Diego Rivera en la Ciudad de México",
+    "autor" => "Fernando López",
+    "resumen" => "Expertos en conservación han comenzado la restauración de uno de los murales más emblemáticos de Diego Rivera, destacando su importancia en la historia del arte mexicano.",
 ];
 ?>
